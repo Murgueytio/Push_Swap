00e0273902c1b77075e5bedf7d802aa3ad2cfd67
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-01-09 14:58:27 +0100

    Add files via upload

---
+++ push_swap.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oscamurg <oscamurg@student.42madrid.com>   #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-01-09 13:10:20 by oscamurg          #+#    #+#             */
+/*   Updated: 2025-01-09 13:10:20 by oscamurg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+
+void	push_stack_a(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+void	push_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+
+void	swap_stack_a(long *stack_a, int *numbers_in_stack_a);
+
+void	rotate_stack_a(long *stack_a, int *numbers_in_stack_a);
+void	rotate_stack_b(long *stack_b, int *numbers_in_stack_b);
+void	rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+
+void	reverse_rotate_stack_a(long *stack_a, int *numbers_in_stack_a);
+
+void	free_stacks(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+void	parameters(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+
+void	sort_3_numbers(long *stack_a, int *numbers_in_stack_a);
+void	sort_5_numbers(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+void	sort_beyond_3_and_5_numbers(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+
+int		ft_strlen(const char *str);
+int		number_limits(long *stack_a, int numbers_in_stack_a);
+int		repeated_numbers(long *stack_a, int numbers_in_stack_a);
+int		is_only_numbers_in_the_stacks(char *numbers);
+int		stack_size(int argc, char **argv);
+int		seperating_input_numbers(long *stack_a, int *numbers_in_stack_a, char **argv);
+int		is_stack_sorted(long *stack_a, int *numbers_in_stack_a);
+
+char	**ft_split(char const *str, char c);
+
+#endif
