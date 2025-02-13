3be40be6e695cf7adae83f663b044b3064f3a19b
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-02-13 13:48:11 +0100

    Update and rename push_swap.h to push.c

---
+++ operations/push.c
@@ -0,0 +1,53 @@
+#include "push_swap.h"
+
+void pa(long *stack_a, long *stack_b, int *numbers_a, int *numbers_b)
+{
+    if (*numbers_b > 0)
+    {
+        int i;
+    
+        i = *numbers_a;
+        while (i > 0)
+        {
+            stack_a[i] = stack_a[i - 1];
+            i--;
+        }
+        stack_a[0] = stack_b[0];
+        (*numbers_a)++;
+        int j;
+        j = 0;
+        while (j < *numbers_b - 1)
+        {
+            stack_b[j] = stack_b[j + 1];
+            j++;
+        }
+        (*numbers_b)--;
+    }
+    write(1, "Push Stack B to A\n", 19);
+}
+
+void pb(long *stack_a, long *stack_b, int *numbers_a, int *numbers_b)
+{
+    if (*numbers_a > 0)
+    {
+        int i;
+    
+        i = *numbers_b;
+        while (i > 0)
+        {
+            stack_b[i] = stack_b[i - 1];
+            i--;
+        }
+        stack_b[0] = stack_a[0];
+        (*numbers_b)++;
+        int j;
+        j = 0;
+        while (j < *numbers_a - 1)
+        {
+            stack_a[j] = stack_a[j + 1];
+            j++;
+        }
+        (*numbers_a)--;
+    }
+    write(1, "Push Stack A to B\n", 19);
+}
