7f2c54d2ac87a0030bd96bd43f123dd61cbb040c
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-02-24 10:56:51 +0100

    Update push.c

--- operations/push.c
+++ operations/push.c
@@ -1,53 +1,27 @@
-#include "push_swap.h"
+#include "../push_swap.h"
 
-void pa(long *stack_a, long *stack_b, int *numbers_a, int *numbers_b)
+void    pa(t_node **stack_a, t_node **stack_b)
 {
-    if (*numbers_b > 0)
-    {
-        int i;
-    
-        i = *numbers_a;
-        while (i > 0)
-        {
-            stack_a[i] = stack_a[i - 1];
-            i--;
-        }
-        stack_a[0] = stack_b[0];
-        (*numbers_a)++;
-        (*numbers_b)--;
-        int j;
-        j = 0;
-        while (j < *numbers_b)
-        {
-            stack_b[j] = stack_b[j + 1];
-            j++;
-        }
-    }
-    write(1, "Push Stack B to A\n", 19);
+    t_node  *temp;
+
+    if (!*stack_b)
+        return ;
+    temp = *stack_b;
+    *stack_b = (*stack_b)->next;
+    temp->next = *stack_a;
+    *stack_a = temp;
+    write(1, "pa\n", 3);
 }
 
-void pb(long *stack_a, long *stack_b, int *numbers_a, int *numbers_b)
+void    pb(t_node **stack_a, t_node **stack_b)
 {
-    if (*numbers_a > 0)
-    {
-        int i;
-    
-        i = *numbers_b;
-        while (i > 0)
-        {
-            stack_b[i] = stack_b[i - 1];
-            i--;
-        }
-        stack_b[0] = stack_a[0];
-        (*numbers_b)++;
-        (*numbers_a)--;
-        int j;
-        j = 0;
-        while (j < *numbers_a)
-        {
-            stack_a[j] = stack_a[j + 1];
-            j++;
-        }
-    }
-    write(1, "Push Stack A to B\n", 19);
+    t_node  *temp;
+
+    if (!*stack_a)
+        return ;
+    temp = *stack_a;
+    *stack_a = (*stack_a)->next;
+    temp->next = *stack_b;
+    *stack_b = temp;
+    write(1, "pb\n", 3);
 }
