2f55ca456d505cd9aaf01405e3090b8913ad4635
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-02-24 11:09:02 +0100

    Update and rename push_swap_utils_2.c to utils.c

---
+++ utils/utils.c
@@ -0,0 +1,95 @@
+#include "../push_swap.h"
+
+// Checks if the stack is already sorted in ascending order.
+int is_sorted(t_node *stack)
+{
+	t_node  *current;
+
+	if (!stack)
+		return (1);
+	current = stack;
+	while (current->next)
+	{
+		if (current->value > current->next->value)
+			return (0);
+		current = current->next;
+	}
+	return (1);
+}
+
+// Returns the index of the smallest element in the stack.
+int get_min_index(long *stack, int size)
+{
+	int i;
+	int min_index;
+
+	min_index = 0;
+	i = 1;
+	while (i < size)
+	{
+		if (stack[i] < stack[min_index])
+			min_index = i;
+		i++;
+	}
+	return (min_index);
+}
+
+void    error_exit(t_node **stack_a, t_node **stack_b)
+{
+	if (stack_a)
+		free_stack(stack_a);
+	if (stack_b)
+		free_stack(stack_b);
+	write(2, "Error\n", 6);
+	exit(EXIT_FAILURE);
+}
+
+long    ft_atoi(const char *str)
+{
+	int     i;
+	int     sign;
+	long    result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		if ((sign == 1 && result > INT_MAX) || 
+			(sign == -1 && result * sign < INT_MIN))
+			return (LONG_MAX);
+		i++;
+	}
+	if (str[i] != '\0')
+		return (LONG_MAX);
+	return (result * sign);
+}
+
+int     check_duplicates(t_node *stack)
+{
+	t_node  *current;
+	t_node  *check;
+
+	current = stack;
+	while (current)
+	{
+		check = current->next;
+		while (check)
+		{
+			if (current->value == check->value)
+				return (1);
+			check = check->next;
+		}
+		current = current->next;
+	}
+	return (0);
+}
