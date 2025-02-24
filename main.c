e05c491324ac22bab4b9d8bd151a98d2ba42707c
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-02-24 15:56:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -81,58 +81,55 @@ static void parse_args(int argc, char **argv, long **stack_a,
         error_exit();
 }
 
-static void init_stacks(t_node **stack_a, char **argv, int argc)
+static void	init_stacks(t_node **stack_a, char **argv, int argc)
 {
-    int     i;
-    long    num;
-    t_node  *new_node;
+	int		i;
+	long	num;
+	t_node	*new_node;
 
-    *stack_a = NULL;
-    i = 1;
-    while (i < argc)
-    {
-        num = ft_atoi(argv[i]);
-        if (num == LONG_MAX || num > INT_MAX || num < INT_MIN)
-            error_exit();
-        new_node = create_node((int)num);
-        if (!new_node)
-            error_exit();
-        add_node_back(stack_a, new_node);
-        i++;
-    }
-    if (check_duplicates(*stack_a))
-        error_exit();
+	*stack_a = NULL;
+	i = 1;
+	while (i < argc)
+	{
+		num = ft_atoi(argv[i]);
+		if (num == LONG_MAX || num > INT_MAX || num < INT_MIN)
+			error_exit();
+		new_node = create_node((int)num);
+		if (!new_node)
+			error_exit();
+		add_node_back(stack_a, new_node);
+		i++;
+	}
+	if (check_duplicates(*stack_a))
+		error_exit();
 }
 
-static void sort_stack(t_node **stack_a, t_node **stack_b)
+static void	sort_stack(t_node **stack_a, t_node **stack_b)
 {
-    int size;
+	int	size;
 
-    if (is_sorted(*stack_a))
-        return;
-    
-    size = stack_size(*stack_a);
-    if (size <= 3)
-        sort_three(stack_a);
-    else if (size <= 5)
-        sort_small(stack_a, stack_b);
-    else
-        sort_big(stack_a, stack_b);
+	if (is_sorted(*stack_a))
+		return ;
+	size = stack_size(*stack_a);
+	if (size <= 3)
+		sort_three(stack_a);
+	else if (size <= 5)
+		sort_small(stack_a, stack_b);
+	else
+		sort_big(stack_a, stack_b);
 }
 
 int main(int argc, char **argv)
 {
-    t_node  *stack_a;
-    t_node  *stack_b;
+	t_node	*stack_a;
+	t_node	*stack_b;
 
-    if (argc < 2)
-        return (0);
-
-    stack_b = NULL;
-    init_stacks(&stack_a, argv, argc);
-    sort_stack(&stack_a, &stack_b);
-
-    free_stack(&stack_a);
-    free_stack(&stack_b);
-    return (0);
+	if (argc < 2)
+		return (0);
+	stack_b = NULL;
+	init_stacks(&stack_a, argv, argc);
+	sort_stack(&stack_a, &stack_b);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
+	return (0);
 }
