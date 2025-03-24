b0fd839c39024bf06b968b63477efd45ca2fe424
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-03-24 08:56:06 +0100

    Update swap.c

--- operations/swap.c
+++ operations/swap.c
@@ -1,38 +1,38 @@
 #include "../push_swap.h"
 
-void    sa(t_node **stack_a)
+void	sa(t_node **stack_a)
 {
-    t_node  *first;
-    t_node  *second;
+	t_node	*first;
+	t_node	*second;
 
-    if (!*stack_a || !(*stack_a)->next)
-        return ;
-    first = *stack_a;
-    second = (*stack_a)->next;
-    first->next = second->next;
-    second->next = first;
-    *stack_a = second;
-    write(1, "sa\n", 3);
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	first = *stack_a;
+	second = (*stack_a)->next;
+	first->next = second->next;
+	second->next = first;
+	*stack_a = second;
+	write(1, "sa\n", 3);
 }
 
-void    sb(t_node **stack_b)
+void	sb(t_node **stack_b)
 {
-    t_node  *first;
-    t_node  *second;
+	t_node	*first;
+	t_node	*second;
 
-    if (!*stack_b || !(*stack_b)->next)
-        return ;
-    first = *stack_b;
-    second = (*stack_b)->next;
-    first->next = second->next;
-    second->next = first;
-    *stack_b = second;
-    write(1, "sb\n", 3);
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	first = *stack_b;
+	second = (*stack_b)->next;
+	first->next = second->next;
+	second->next = first;
+	*stack_b = second;
+	write(1, "sb\n", 3);
 }
 
-void    ss(t_node **stack_a, t_node **stack_b)
+void	ss(t_node **stack_a, t_node **stack_b)
 {
-    sa(stack_a);
-    sb(stack_b);
-    write(1, "ss\n", 3);
+	sa(stack_a);
+	sb(stack_b);
+	write(1, "ss\n", 3);
 }
