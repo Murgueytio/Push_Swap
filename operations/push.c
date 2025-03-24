9674646fae34633bc8106de563eed9afa7da0fff
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-03-24 08:52:31 +0100

    Update push.c

--- operations/push.c
+++ operations/push.c
@@ -1,27 +1,27 @@
 #include "../push_swap.h"
 
-void    pa(t_node **stack_a, t_node **stack_b)
+void	pa(t_node **stack_a, t_node **stack_b)
 {
-    t_node  *temp;
+	t_node	*temp;
 
-    if (!*stack_b)
-        return ;
-    temp = *stack_b;
-    *stack_b = (*stack_b)->next;
-    temp->next = *stack_a;
-    *stack_a = temp;
-    write(1, "pa\n", 3);
+	if (!*stack_b)
+		return ;
+	temp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	temp->next = *stack_a;
+	*stack_a = temp;
+	write(1, "pa\n", 3);
 }
 
-void    pb(t_node **stack_a, t_node **stack_b)
+void	pb(t_node **stack_a, t_node **stack_b)
 {
-    t_node  *temp;
+	t_node	*temp;
 
-    if (!*stack_a)
-        return ;
-    temp = *stack_a;
-    *stack_a = (*stack_a)->next;
-    temp->next = *stack_b;
-    *stack_b = temp;
-    write(1, "pb\n", 3);
+	if (!*stack_a)
+		return ;
+	temp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	temp->next = *stack_b;
+	*stack_b = temp;
+	write(1, "pb\n", 3);
 }
