65884f6669501c3f7b046159835fd8818a63b344
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-03-24 09:05:14 +0100

    Update sort_big.c

--- sorting/sort_big.c
+++ sorting/sort_big.c
@@ -24,9 +24,13 @@ static void	normalize_stack(t_node **stack)
 
 static int	get_chunk_size(int size)
 {
-	if (size <= 100)
-		return (15);
-	return (30);
+	if (size <= 20)
+		return (5);
+	else if (size <= 100)
+		return (20); // Con 6 se acerca a los 800 y sin 'size/'
+	else if (size <= 500)
+		return (8 + size / 14);
+	return (42);
 }
 
 static int	get_min_pos(t_node *stack, int start, int end)
@@ -34,21 +38,21 @@ static int	get_min_pos(t_node *stack, int start, int end)
 	int		pos;
 	int		min_pos;
 	int		min;
-	t_node	*current;
+	t_node	*now;
 
 	pos = 0;
 	min_pos = -1;
 	min = INT_MAX;
-	current = stack;
-	while (current)
+	now = stack;
+	while (now)
 	{
-		if (current->value >= start && current->value <= end && current->value < min)
+		if (now->value >= start && now->value <= end && now->value < min)
 		{
-			min = current->value;
+			min = now->value;
 			min_pos = pos;
 		}
 		pos++;
-		current = current->next;
+		now = now->next;
 	}
 	return (min_pos);
 }
@@ -64,7 +68,7 @@ static void	push_chunk(t_node **stack_a, t_node **stack_b, int start, int end)
 	{
 		pos = get_min_pos(*stack_a, start, end);
 		if (pos == -1)
-			break ;
+			break;
 		middle = size / 2;
 		while (pos != 0)
 		{
@@ -75,6 +79,9 @@ static void	push_chunk(t_node **stack_a, t_node **stack_b, int start, int end)
 			pos = get_min_pos(*stack_a, start, end);
 		}
 		pb(stack_a, stack_b);
+		if (*stack_b && (*stack_b)->next &&
+			(*stack_b)->value < (*stack_b)->next->value)
+			sb(stack_b);
 		size = stack_size(*stack_a);
 	}
 }
@@ -82,6 +89,7 @@ static void	push_chunk(t_node **stack_a, t_node **stack_b, int start, int end)
 void	sort_big(t_node **stack_a, t_node **stack_b)
 {
 	int	size;
+	int	chunk_count;
 	int	chunk_size;
 	int	i;
 	int	start;
@@ -90,19 +98,20 @@ void	sort_big(t_node **stack_a, t_node **stack_b)
 		return ;
 	size = stack_size(*stack_a);
 	if (size <= 5)
-	{
-		sort_small(stack_a, stack_b);
-		return ;
-	}
+		return (sort_small(stack_a, stack_b));
 	normalize_stack(stack_a);
-	chunk_size = get_chunk_size(size);
+	if (size <= 100)
+		chunk_count = 33;  // Para 100 números, usamos 5 chunks
+	else
+		chunk_count = size / get_chunk_size(size);
+	chunk_size = size / chunk_count;
 	i = 0;
-	while (i < size)
+	while (i++ < chunk_count)
 	{
-		start = i;
+		start = i * chunk_size;
 		push_chunk(stack_a, stack_b, start, start + chunk_size - 1);
-		i += chunk_size;
 	}
 	while (*stack_b)
 		pa(stack_a, stack_b);
 }
+
