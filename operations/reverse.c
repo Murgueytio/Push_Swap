f1cde302fae9f14429ce05fc344ddbaef6666e2d
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-03-24 08:55:28 +0100

    Update reverse.c

--- operations/reverse.c
+++ operations/reverse.c
@@ -1,48 +1,48 @@
 #include "../push_swap.h"
 
-void    rra(t_node **stack_a)
+void	rra(t_node **stack_a)
 {
-    t_node  *last;
-    t_node  *second_last;
+	t_node	*last;
+	t_node	*second_last;
 
-    if (!*stack_a || !(*stack_a)->next)
-        return ;
-    last = *stack_a;
-    second_last = NULL;
-    while (last->next)
-    {
-        second_last = last;
-        last = last->next;
-    }
-    second_last->next = NULL;
-    last->next = *stack_a;
-    *stack_a = last;
-    write(1, "rra\n", 4);
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	last = *stack_a;
+	second_last = NULL;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->next = *stack_a;
+	*stack_a = last;
+	write(1, "rra\n", 4);
 }
 
-void    rrb(t_node **stack_b)
+void	rrb(t_node **stack_b)
 {
-    t_node  *last;
-    t_node  *second_last;
+	t_node	*last;
+	t_node	*second_last;
 
-    if (!*stack_b || !(*stack_b)->next)
-        return ;
-    last = *stack_b;
-    second_last = NULL;
-    while (last->next)
-    {
-        second_last = last;
-        last = last->next;
-    }
-    second_last->next = NULL;
-    last->next = *stack_b;
-    *stack_b = last;
-    write(1, "rrb\n", 4);
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	last = *stack_b;
+	second_last = NULL;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->next = *stack_b;
+	*stack_b = last;
+	write(1, "rrb\n", 4);
 }
 
-void    rrr(t_node **stack_a, t_node **stack_b)
+void	rrr(t_node **stack_a, t_node **stack_b)
 {
-    rra(stack_a);
-    rrb(stack_b);
-    write(1, "rrr\n", 4);
+	rra(stack_a);
+	rrb(stack_b);
+	write(1, "rrr\n", 4);
 }
