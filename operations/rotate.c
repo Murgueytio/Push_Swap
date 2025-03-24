e0a8bfe4cbac0f9561b469a9012b8ce3e731dc21
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2025-03-24 08:54:47 +0100

    Update rotate.c

--- operations/rotate.c
+++ operations/rotate.c
@@ -1,42 +1,38 @@
 #include "../push_swap.h"
 
-void    ra(t_node **stack_a)
+void	ra(t_node **stack_a)
 {
-    t_node  *first;
-    t_node  *last;
+	t_node	*first;
+	t_node	*last;
 
-    if (!*stack_a || !(*stack_a)->next)
-        return ;
-    first = *stack_a;
-    last = *stack_a;
-    while (last->next)
-        last = last->next;
-    *stack_a = first->next;
-    first->next = NULL;
-    last->next = first;
-    write(1, "ra\n", 3);
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	first = *stack_a;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	*stack_a = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1, "ra\n", 3);
 }
 
-void    rb(t_node **stack_b)
+void	rb(t_node **stack_b)
 {
-    t_node  *first;
-    t_node  *last;
+	t_node	*first;
+	t_node	*last;
 
-    if (!*stack_b || !(*stack_b)->next)
-        return ;
-    first = *stack_b;
-    last = *stack_b;
-    while (last->next)
-        last = last->next;
-    *stack_b = first->next;
-    first->next = NULL;
-    last->next = first;
-    write(1, "rb\n", 3);
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	first = *stack_b;
+	last = *stack_b;
+	while (last->next)
+		last = last->next;
 }
 
-void    rr(t_node **stack_a, t_node **stack_b)
+void	rr(t_node **stack_a, t_node **stack_b)
 {
-    ra(stack_a);
-    rb(stack_b);
-    write(1, "rr\n", 3);
+	ra(stack_a);
+	rb(stack_b);
+	write(1, "rr\n", 3);
 }
